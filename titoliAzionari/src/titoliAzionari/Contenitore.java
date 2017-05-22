9d40bbdc805102405b75e9fbf7e3a797dc7ec7e0
Ghidoit97 <f.ghidini011@studenti.unibs.it>
2017-05-22 15:00:08 +0200

    upload Contenitore

--- titoliAzionari/src/titoliAzionari/Contenitore.java
+++ titoliAzionari/src/titoliAzionari/Contenitore.java
@@ -1,5 +1,24 @@
 package titoliAzionari;
 
 public class Contenitore {
+	
+	private ElencoTitoli elencoTitoli;
+	private Portafoglio portafoglio;
+	
+	public Contenitore(ElencoTitoli elencoTitoli, Portafoglio portafoglio)
+	{
+		this.elencoTitoli=elencoTitoli;
+		this.portafoglio=portafoglio;
+	}
+	
+	public ElencoTitoli getElencoTitoli()
+	{
+		return elencoTitoli;
+	}
+	
+	public Portafoglio getPortafoglio()
+	{
+		return portafoglio;
+	}
 
 }
